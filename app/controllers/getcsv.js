6736054d726e4c07b9b1a75002a2474e1cb58f4e
jpfeiffer <joepfeiffer16@gmail.com>
2015-06-30 23:38:09 -0600

    Adding a simple authToken

--- app/controllers/getcsv.js
+++ app/controllers/getcsv.js
@@ -1,3 +1,4 @@
+/* global process */
 var express = require('express'),
   router = express.Router();
   
@@ -8,18 +9,39 @@ module.exports = function (app) {
 
 var mongo = require('../models/mongo-functions/mongo-helper.js');
 router.post('/getcsv', function(req, res, next) {
-  var fileTitle = Date.now.toString() + '.csv';
   
-  mongo.getResponses(function (responseList){
-    var csvString = 'Name,Attending,Number' + String.fromCharCode(10);
+  var authToken;
+  
+  try {
+    authToken = require('../../config/enviro.js');
+  } catch(e) {
+    if (process.env.authToken) {
+      authToken = process.env.authToken;
+    } else {
+      throw e;
+    }
+  }
+  
+  
+  if (req.body.authToken = authToken) {
+  
     
-  	for (var i = 1; i < responseList.length; i++) {
-  		var rsvp = responseList[i];
-  		var name = rsvp.name;
-  		var attending = rsvp.attending;
-  		var numberAttending = rsvp.numberAttending;
-  		csvString = csvString + name + ',' + attending + ',' + numberAttending + String.fromCharCode(10);
-  	}
-    res.send(csvString);
-  });  
+    var fileTitle = Date.now.toString() + '.csv';
+    
+    mongo.getResponses(function (responseList){
+      var csvString = 'Name,Attending,Number' + String.fromCharCode(10);
+      
+    	for (var i = 1; i < responseList.length; i++) {
+    		var rsvp = responseList[i];
+    		var name = rsvp.name;
+    		var attending = rsvp.attending;
+    		var numberAttending = rsvp.numberAttending;
+    		csvString = csvString + name + ',' + attending + ',' + numberAttending + String.fromCharCode(10);
+    	}
+      res.send(csvString);
+    });
+  } else {
+    res.status(500);
+    res.send('Error. Invalid Authentication Token');
+  }
 });
