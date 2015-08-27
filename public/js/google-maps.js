08c0f4696d44e76db1ecb41165fc46d9b7cc4ed9
Joe Pfeiffer <jpfeiffer@bluemodus.com>
2015-08-27 11:52:05 -0600

    Google Maps

--- public/js/google-maps.js
+++ public/js/google-maps.js
@@ -3,10 +3,14 @@ function initMap() {
   var directionsDisplay = new google.maps.DirectionsRenderer;
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 7,
-    center: {lat: 41.85, lng: -87.65}
+    // center: {lat: 41.85, lng: -87.65}
+    center: {lat: 39.742431, lng: -104.681904}
+    //39.742431, -104.681904
   });
   directionsDisplay.setMap(map);
 
+  calculateAndDisplayRoute(directionsService, directionsDisplay);
+  
   // var onChangeHandler = function() {
   //   calculateAndDisplayRoute(directionsService, directionsDisplay);
   // };
@@ -18,8 +22,8 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
   directionsService.route({
     // origin: document.getElementById('start').value,
     // destination: document.getElementById('end').value,
-    origin: 'Chicago',
-    destination: 'Louisville',
+    origin: '32100 E Colfax AveWatkins, CO 80137',
+    destination: 'La Quinta Inn & Suites Denver Airport DIA6801 Tower Rd Denver, CO 80249',
     travelMode: google.maps.TravelMode.DRIVING
   }, function(response, status) {
     if (status === google.maps.DirectionsStatus.OK) {
